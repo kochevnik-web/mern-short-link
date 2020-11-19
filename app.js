a280102f8135b64804b40e57adfc2c3619a60f62
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-11-19 12:42:26 +0300

    create conntect mongo db

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const PORT = config.get('port') || 5000;
 
 const app = express();
 
-app.listen(PORT, ()=> console.log(`Server has been started on port ${PORT}...`));
+async function start(){
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, ()=> console.log(`Server has been started on port ${PORT}...`));
+    } catch (e) {
+        console.log('Server error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
